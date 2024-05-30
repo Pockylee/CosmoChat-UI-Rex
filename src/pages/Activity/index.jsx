94673c5bb0b5e324e873b12ec5332d93571d4edd
Pockylee <brian7149@gmail.com>
2024-05-30 15:58:04 +0800

    Acticity/index.jsx done

--- src/pages/Activity/index.jsx
+++ src/pages/Activity/index.jsx
@@ -12,5 +12,63 @@ const Activity = () => {
     const [sessionChatLengths, setSessionChatLengths] = useState([]);
     const [sessions, setSessions] = useState([]);
     
-    
-}
+    useEffect(() => {
+        const fetchSessions = async () => {
+            try {
+                const response = await api.get("/sessions");
+                setSessions(response.data.reverse());
+                setSessionDates(
+                    [...Array.from(response.data, (data) => data.date.split(",")[0])]
+                )
+                setSessionChatLengths(
+                    [...Array.from(response.data, (data) => data.chats.length)]
+                )
+                setLoading(false);
+            } catch (err) {
+                if (err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(err);
+                }
+            }
+        };
+        fetchSessions();
+    }, []);
+    return (
+        <Grid container className={ActivityStyles.activityBody}>
+          <Grid container item className={ActivityStyles.titleOutline}>
+            <Typography className={ActivityStyles.title}>Your Statistics</Typography>
+          </Grid>
+          <Grid container item>
+            <Typography className={ActivityStyles.description}>
+              Graph of the conversation you had with ReX this year.
+            </Typography>
+          </Grid>
+          <Grid container item>
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              <BarChart
+                xAxis={{ scaleType: "band", data: sessionDates }}
+                series={[{ data: sessionChatLengths }]}
+                width={500}
+                height={300}
+              />
+            )}
+          </Grid>
+          <Grid container item className={AllStyles.endedChatsTitle}>
+            <Grid className={AllStyles.endedChats}>Details Chat Activity</Grid>
+            <Link className={AllStyles.seeAllLink} href="/activityDetails">
+              See All
+            </Link>
+          </Grid>
+          <Grid container item>
+            {loading ? <CircularProgress /> : <ChatHistory sessions={sessions} />}
+          </Grid>
+        </Grid>
+      );
+};
+
+export default Activity;
